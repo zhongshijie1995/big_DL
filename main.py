eb110da74725ce6c960f4082112c8c6520c549c9
zhongshijie <zhongshijie1995@outlook.com>
2023-08-27 11:26:59 +0800

    完成功能：人+手机发出红色警报，修改检测频率

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from _logic import singe_frame
 if __name__ == '__main__':
     task_func_list = [singe_frame.say_hello, ]
     task_func_args_list = ["显示目标", ]
-    local_capture.realtime(3, task_func_list, task_func_args_list)
+    local_capture.realtime(1, task_func_list, task_func_args_list)
